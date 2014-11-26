c4d77f1a06093207253ba4f2fa3653c8774595dd
jbeatj <jbeatj@hotmail.com>
2014-11-26 13:37:50 -0500

    Ajout zone de tir

--- unity-version/Assets/laser_script.cs
+++ unity-version/Assets/laser_script.cs
@@ -16,6 +16,7 @@ public class laser_script : MonoBehaviour
 
 
 	private GameObject lineRenderer;
+	private Rect shootingZone = new Rect(0, 0, Screen.width, Screen.height / 3);
 
 	void Start ()
 	{
@@ -27,42 +28,44 @@ public class laser_script : MonoBehaviour
 	{        	
 		Touch t = Input.GetTouch (0);
 
-		if (t.phase == TouchPhase.Began) {
-			if (Time.time >= nextShot) {
-				firstFingerPos = t.position;
-				objFirstpos = Camera.main.ScreenToWorldPoint (firstFingerPos);
-				objFirstpos.z = 0.0f;// Sinon Z est a -10.
+		if (shootingZone.Contains(t.position)) {
+
+			if (t.phase == TouchPhase.Began) {
+				if (Time.time >= nextShot) {
+					firstFingerPos = t.position;
+					objFirstpos = Camera.main.ScreenToWorldPoint (firstFingerPos);
+					objFirstpos.z = 0.0f;// Sinon Z est a -10.
+				}
 			}
+				
+	         if(t.phase == TouchPhase.Ended){
+				if (Time.time >= nextShot) {
+
+					Vector3 endFingerPos = t.position;
+					Vector3 objPos = Camera.main.ScreenToWorldPoint (endFingerPos);
+					objPos.z = 0.0f;// Sinon Z est a -10.
+
+					//SCALING
+					float scalingy = objFirstpos.y - objPos.y;
+					Debug.Log (scalingy);
+
+					Transform local_laser_pref = laser_pref;
+					local_laser_pref.localScale = new Vector3(0.15f, scalingy, 1);
+					//*******
+
+
+					Vector3 vectorToTarget = endFingerPos - firstFingerPos;
+					float angle = Mathf.Atan2 (vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg;
+					Quaternion rotation = new Quaternion ();
+					rotation.eulerAngles = new Vector3 (0, 0,angle-90);
+
+
+					audio.PlayOneShot (laserSound, 0.5f);
+					nextShot = Time.time;
+					Rigidbody laser = (Rigidbody) Instantiate (local_laser_pref, objPos, rotation);
+				}
+	          }
 		}
-			
-         if(t.phase == TouchPhase.Ended){
-			if (Time.time >= nextShot) {
-
-				Vector3 endFingerPos = t.position;
-				Vector3 objPos = Camera.main.ScreenToWorldPoint (endFingerPos);
-				objPos.z = 0.0f;// Sinon Z est a -10.
-
-				//SCALING
-				float scalingy = objFirstpos.y - objPos.y;
-				Debug.Log (scalingy);
-
-				Transform local_laser_pref = laser_pref;
-				local_laser_pref.localScale = new Vector3(0.20f, scalingy, 1);
-				//*******
-
-
-				Vector3 vectorToTarget = endFingerPos - firstFingerPos;
-				float angle = Mathf.Atan2 (vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg;
-				Quaternion rotation = new Quaternion ();
-				rotation.eulerAngles = new Vector3 (0, 0,angle-90);
-
-
-				audio.PlayOneShot (laserSound, 0.5f);
-				nextShot = Time.time;
-				Rigidbody laser = (Rigidbody) Instantiate (local_laser_pref, objPos, rotation);
-			}
-          }
-
 	}
 
      	
