98e8bc445f31123077389e1e7a3ff5bf0cdffa52
jbeatj <jbeatj@hotmail.com>
2014-12-05 15:56:04 -0500

    viseur

---
+++ unity-version/Assets/rotatingAim.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotatingAim : MonoBehaviour {
+	public GameObject aim;
+
+	// Use this for initialization
+	void Start () {
+		GameObject aimeClone = (GameObject) Instantiate (aim, this.transform.position, this.transform.rotation);
+		aimeClone.transform.parent = transform;
+		aimeClone.transform.localPosition = new Vector3 (0, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
