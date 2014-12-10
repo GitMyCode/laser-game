149ce0042cffc137fee851c3c2640fe43277d615
GitMyCode <marbouchl@gmail.com>
2014-12-10 00:56:08 -0500

    Ajout de la fonctionnalit  avec cercle

---
+++ unity-version/Assets/DeathTimer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathTimer : MonoBehaviour {
+
+	public float timeOut = 1.0f;
+	public bool detachChildren = false;
+
+	// Use this for initialization
+	void Awake () {
+	
+		Invoke ("DestroyNow", timeOut);
+	}
+
+	private void DestroyNow ()
+	{
+		if (detachChildren) {
+			transform.DetachChildren ();
+		}
+		DestroyObject (gameObject);
+	}
+}
