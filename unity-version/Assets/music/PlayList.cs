fc222cced084e3d4f89897c332ac2ead3c633022
jbeatj <jbeatj@hotmail.com>
2014-12-12 15:10:27 -0500

    beat

---
+++ unity-version/Assets/music/PlayList.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayList : MonoBehaviour {
+
+	public AudioClip[] myMusic = new AudioClip[3];
+	
+	void Update() { 
+
+		if(!audio.isPlaying) playRandomMusic(); 
+	}
+	
+	void playRandomMusic() { 
+		audio.clip = myMusic[Random.Range(0, myMusic.Length)]; audio.Play(); 
+	}
+}
