af95ed52c1a3a189765530ee4026c3cbdae31035
Martin Bouchard <marbouchl@gmail.com>
2014-12-28 17:36:49 -0500

    quick fix action avant un autre refactor

---
+++ unity-version/Assets/Scripts/Models/Actions/EAction.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum EAction {
+    
+    NONE = 0,
+    ABSORBE = 1,
+    BLOCK = 2,
+    LINE = 4
+
+}
---
+++ unity-version/Assets/Scripts/Models/CircleModel.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CircleModel {
+
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public List<GameObject> findZoneContainingForCircle(Vector3 touchposition)
+    {
+
+        List<GameObject> allLines = new List<GameObject>();
+
+        DebugDraw.DrawSphere(touchposition, (GameArbiter.Instance.transform.lossyScale.x), Color.red);
+        RaycastHit2D[] hit = Physics2D.CircleCastAll(
+            touchposition,
+            GameArbiter.Instance.transform.lossyScale.x / 2
+            , Vector2.zero);
+
+        for (int i = 0; i < hit.Length; i++)
+        {
+            if (hit[i].collider.tag.Equals("lineHead"))
+            {
+                allLines.Add(hit[i].collider.gameObject);
+            }
+        }
+        return allLines;
+    }
+
+}
