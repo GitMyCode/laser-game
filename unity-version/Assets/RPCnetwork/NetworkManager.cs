e94923b2ec506d6ac6ee6e4055f06d47c977ee71
jbeatj <jbeatj@hotmail.com>
2015-01-01 12:10:06 -0500

    Rpc to implement

---
+++ unity-version/Assets/RPCnetwork/NetworkManager.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkManager : MonoBehaviour {
+
+    private const string typeName = "LINEWARMULTITEST";
+    private const string gameName = "Jennifer";
+    private VectorGrid m_vectorGrid2;
+    //public GameObject playerPrefab;
+
+    private void StartServer()
+    {
+        Network.InitializeServer(4, 25000, !Network.HavePublicAddress());
+        MasterServer.RegisterHost(typeName, gameName);
+    }
+
+    void OnServerInitialized()
+    {
+       SpawnPlayer();
+    }
+
+
+    void OnGUI()
+
+    {
+        if (!Network.isClient && !Network.isServer)
+        {
+            if (GUI.Button(new Rect(100, 100, 250, 100), "Start Server"))
+                StartServer();
+
+            if (GUI.Button(new Rect(100, 250, 250, 100), "Refresh Hosts"))
+                RefreshHostList();
+
+            if (hostList != null)
+            {
+                for (int i = 0; i < hostList.Length; i++)
+                {
+                    if (GUI.Button(new Rect(400, 100 + (110 * i), 300, 100), hostList[i].gameName))
+                        JoinServer(hostList[i]);
+                }
+            }
+        }
+    }
+
+
+    private HostData[] hostList;
+
+    private void RefreshHostList()
+    {
+        MasterServer.RequestHostList(typeName);
+    }
+
+    void OnMasterServerEvent(MasterServerEvent msEvent)
+    {
+        if (msEvent == MasterServerEvent.HostListReceived)
+            hostList = MasterServer.PollHostList();
+    }
+
+    private void JoinServer(HostData hostData)
+    {
+        Network.Connect(hostData);
+    }
+
+    void OnConnectedToServer()
+    {
+        SpawnPlayer();
+    }
+    
+    private void SpawnPlayer()
+    {
+       // Network.Instantiate(playerPrefab, new Vector3(0.8f, 0.95f, 2.54209f), Quaternion.identity, 0);
+        
+    }
+    
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
