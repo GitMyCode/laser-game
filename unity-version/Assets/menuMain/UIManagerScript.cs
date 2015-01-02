66118133839f2ed17284434f62bff6cbf74d3103
jbeatj <jbeatj@hotmail.com>
2015-01-02 01:09:57 -0500

    Update UIManagerScript.cs

--- unity-version/Assets/menuMain/UIManagerScript.cs
+++ unity-version/Assets/menuMain/UIManagerScript.cs
@@ -3,18 +3,14 @@ using System.Collections;
 
 public class UIManagerScript : MonoBehaviour {
 
-    public static StateBase state;
 
 	public void StartGameSingle()
 	{
-        state = new StateSingle();
         Application.LoadLevel("LineWar");
 	}
 
     public void StartGameMultiplayer()
     {
-        state = new StateMultiplayer();
-		Application.LoadLevel("LineWar");
     }
 
 }
